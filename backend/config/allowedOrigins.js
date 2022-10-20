45d142f5f73527c68309b9f1fdac939aa4a14197
Shafiul23 <shafiulmirza@hotmail.com>
2022-10-20 17:53:30 +0100

    allowed origins update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     "https://technotes-server.onrender.com",
-    "https://technotes-33dz.onrender.com"
+    "https://www.technotes-server.onrender.com",
+    "https://technotes-33dz.onrender.com",
+    "https://www.technotes-33dz.onrender.com"
 ]
 
 module.exports = allowedOrigins
