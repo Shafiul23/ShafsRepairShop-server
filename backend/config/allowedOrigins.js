c95703a8dac3fa8a9d6591b814794624b9134da8
Shafiul23 <shafiulmirza@hotmail.com>
2022-10-20 15:41:26 +0100

    allowed client name on origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    "https://technotes-server.onrender.com"
+    "https://technotes-server.onrender.com",
+    "https://technotes-33dz.onrender.com"
 ]
 
 module.exports = allowedOrigins
